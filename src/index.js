e4ba8be56f4e8f5ccb67b10826030de18d91fe36
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-03 23:24:41 -0600

    movie item reducer set with dispatch

--- src/index.js
+++ src/index.js
@@ -80,6 +80,17 @@ const SearchOmdbApi = (state = [], action) => {
     }
 }
 
+// store movie item 
+const movieItem = (state = {}, action) => {
+    switch(action.type){
+        case 'SET_MOVIE':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+
 // Create one store that all components can use
 const storeInstance = createStore(
     combineReducers({
