0ad772055ce21cba7265b5d01622f3b8fb6c2a00
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-03 22:09:04 -0600

    compl setup for search router to get search query from OMDb API

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -6,15 +6,15 @@ const axios = require('axios');
 router.get('/:t', (req,res) => {
     const searchTerms  = req.params.t;
     console.log('/search GET', searchTerms);
-
     axios.get(`http://www.omdbapi.com/?t=${searchTerms}&apikey=${process.env.OMDb_API_KEY}`
     )
     .then ((response) => {
-        console.log('response object is', response );
+        console.log('response', response );
+        // console.log('response.data', response.data );
         res.send(response.data);
     })
     .catch((error) => {
-        console.log('ERROR WITH OMDB API GET', error); 
+        console.log('ERROR IN OMDB API GET', error); 
     });
 })
 
