32379c857e21a7772b8f92c605037fa454d713d5
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-04 01:37:08 -0600

    update move detail logic to show page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -6,16 +6,31 @@ import { useHistory, useParams } from 'react-router-dom';
 export default function MovieDetails() {
   const dispatch = useDispatch();
   const history = useHistory();
+  const movies = useSelector(store => store.movies)
+  const genres = useSelector(store => store.genres)
   const { id } = useParams();
   
-  const movieDetail = useSelector(store => store.movies.find(movie => movie.id === Number(id)));
-  const genres = useSelector(store => store.genres);
-  console.log('movieDetail =======', movieDetail);
+
+  // const movieDetail = useSelector(store => store.movies.find(movie => movie.id === Number(id)));
+  // const genres = useSelector(store => store.genres);
+  // console.log('movieDetail =======', movieDetail);
 
   // dispatch({ type: 'FETCH_MOVIE_GENRES', payload: id});
   // useEffect(() => {
   //   dispatch(fetchMovieGenres(id));
   // }, [dispatch, id]);
+
+  let movieDetail = {};
+    function findMovieDetail()  {
+        // console.log('ID is ', id);
+        for (let movie of movies) {
+            if (movie.id === Number(id)) {
+                movieDetail = movie;
+            } 
+        } 
+    }
+
+    findMovieDetail();
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIE_GENRES', payload: id });
