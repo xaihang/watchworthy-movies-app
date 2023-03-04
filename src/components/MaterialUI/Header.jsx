17077e9a9ac57714b920260152618bb6855fbe3b
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-04 15:04:21 -0600

    added mui to header component

--- src/components/MaterialUI/Header.jsx
+++ src/components/MaterialUI/Header.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    letterSpacing: '0.1em',
+    fontFamily: 'sans-serif',
+    fontSize: '2rem',
+    textShadow: '2px 2px 4px #000000',
+  },
+  logo: {
+    width: '50px',
+    height: '50px',
+    marginRight: '10px',
+  },
+}))
 
 export default function Header() {
+  const classes = useStyles()
+
   return (
-    <h1>Watchworthy</h1>
+    <div className={classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <img src="/logo.png" alt="Watchworthy logo" className={classes.logo} />
+          <Typography variant="h6" className={classes.title}>
+            Watchworthy
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
