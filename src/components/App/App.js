0fe424a9f9fbd39ac5c2b452b1af67735450ff91
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-03 22:57:54 -0600

    searchmovie added form, use history, define state of title, genre, yr, descrp, setup dispatch to saga/redux to fetch api search query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,22 +2,30 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import SearchMovie from '../SearchMovie/SearchMovie';
+import MovieDetails from '../MovieDetails/MovieDetails';
+import AddMovie from '../AddMovie/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>     
+      <Router>   
+        <SearchMovie />  
       <Route path="/" exact>
-          <SearchMovie />
+          <MovieList />
         </Route>   
         <Route path="/movielist">
           <MovieList />
         </Route>
-        
+  
         {/* Details page */}
-
+        <Route path="/moviedetails">
+          <MovieDetails />
+        </Route>
         {/* Add Movie page */}
+        <Route path="/addmovie">
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 export default function SearchMovie() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [genre, setGenre] = useState("");
+  const [year, setYear] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // console.log('clicked it');
+
+    dispatch({
+      type: "FETCH_OMDBAPI_RESULTS",
+      payload: {
+        title,
+        year,
+        description,
+        genre,
+      },
+    });
+
+    setTitle("");
+    setYear("");
+    setDescription("");
+    setGenre("");
+  };
+
+  console.log(title, year, description, genre);
+
   return (
-    <div>SearchMovie</div>
-  )
+    <div>
+      <form onSubmit={(e) => handleClick(e)}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="enter movie title here..."
+        />
+
+        <button onClick={() => history.push("/addmovie")}>Search!</button>
+      </form>
+    </div>
+  );
 }
