31d42db1680aa69d8c7cdec035daa5410a047b71
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-04 18:27:30 -0600

    search buttn style with mui

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -5,7 +5,6 @@ import { useHistory } from "react-router-dom";
 import { TextField, Button } from "@material-ui/core";
 import "./SearchMovie.css";
 
-
 export default function SearchMovie() {
   const history = useHistory();
   const dispatch = useDispatch();
@@ -39,93 +38,97 @@ export default function SearchMovie() {
   return (
     <div className="form-container">
       <form onSubmit={(e) => handleClick(e)}>
-      <TextField
+        <TextField
           label="Movie Title"
           variant="outlined"
           size="medium"
           fullWidth
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="form-textfield" 
+          className="form-textfield"
         />
-        <button onClick={() => history.push("/addmovie")}>Search!</button>
+
+        <Button
+          className="search-button"
+          variant="outlined"
+          onClick={() => history.push("/addmovie")}
+        >
+          Search
+        </Button>
       </form>
     </div>
   );
-
 }
-
 
 //! original code that works:
 // return (
-  //   <div>
-  //     <form onSubmit={(e) => handleClick(e)}>
-  //       <input
-  //         type="text"
-  //         value={title}
-  //         onChange={(e) => setTitle(e.target.value)}
-  //         placeholder="movie title..."
-  //       />
-  //       <button onClick={() => history.push("/addmovie")}>Search!</button>
-  //     </form>
-  //   </div>
-  // );
+//   <div>
+//     <form onSubmit={(e) => handleClick(e)}>
+//       <input
+//         type="text"
+//         value={title}
+//         onChange={(e) => setTitle(e.target.value)}
+//         placeholder="movie title..."
+//       />
+//       <button onClick={() => history.push("/addmovie")}>Search!</button>
+//     </form>
+//   </div>
+// );
 
+// return (
+//   <div>
+//     <form onSubmit={(e) => handleClick(e)}>
+//       <input
+//         type="text"
+//         value={title}
+//         onChange={(e) => setTitle(e.target.value)}
+//         placeholder="movie title..."
+//       />
+//       <button onClick={() => history.push("/addmovie")}>Search!</button>
+//     </form>
+//   </div>
+// );
 
-  // return (
-  //   <div>
-  //     <form onSubmit={(e) => handleClick(e)}>
-  //       <input
-  //         type="text"
-  //         value={title}
-  //         onChange={(e) => setTitle(e.target.value)}
-  //         placeholder="movie title..."
-  //       />
-  //       <button onClick={() => history.push("/addmovie")}>Search!</button>
-  //     </form>
-  //   </div>
-  // );
+// material ui stuff:
 
-  // material ui stuff:
-
-  // return (
-  //   <div>
-  //     <form onSubmit={(e) => handleClick(e)}>
-  //       <TextField
-  //         label="Movie Title"
-  //         variant="outlined"
-  //         size="medium"
-  //         fullWidth
-  //         value={title}
-  //         onChange={(e) => setTitle(e.target.value)}
-  //         style={{ marginBottom: 20 }}
-  //       />
-        // <TextField
-        //   label="Genre"
-        //   variant="outlined"
-        //   size="medium"
-        //   fullWidth
-        //   value={genre}
-        //   onChange={(e) => setGenre(e.target.value)}
-        //   style={{ marginBottom: 20 }}
-        // />
-        // <TextField
-        //   label="Year"
-        //   variant="outlined"
-        //   size="medium"
-        //   fullWidth
-        //   value={year}
-        //   onChange={(e) => setYear(e.target.value)}
-        //   style={{ marginBottom: 20 }}
-        // />
-  //       <Button
-  //         variant="contained"
-  //         color="primary"
-  //         onClick={() => history.push("/addmovie")}
-  //         style={{ textTransform: "none", borderRadius: 20, padding: "10px 30px", marginLeft: 20 }}
-  //       >
-  //         Search
-  //       </Button>
-  //     </form>
-  //   </div>
-  // );
+// return (
+//   <div>
+//     <form onSubmit={(e) => handleClick(e)}>
+//       <TextField
+//         label="Movie Title"
+//         variant="outlined"
+//         size="medium"
+//         fullWidth
+//         value={title}
+//         onChange={(e) => setTitle(e.target.value)}
+//         style={{ marginBottom: 20 }}
+//       />
+// <TextField
+//   label="Genre"
+//   variant="outlined"
+//   size="medium"
+//   fullWidth
+//   value={genre}
+//   onChange={(e) => setGenre(e.target.value)}
+//   style={{ marginBottom: 20 }}
+// />
+// <TextField
+//   label="Year"
+//   variant="outlined"
+//   size="medium"
+//   fullWidth
+//   value={year}
+//   onChange={(e) => setYear(e.target.value)}
+//   style={{ marginBottom: 20 }}
+// />
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         onClick={() => history.push("/addmovie")}
+//         style={{ textTransform: "none", borderRadius: 20, padding: "10px 30px", marginLeft: 20 }}
+//       >
+//         Search
+//       </Button>
+//     </form>
+//   </div>
+// );
