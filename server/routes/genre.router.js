ba19b4817e81db9a7e4852c0479f48ae693308be
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-05 13:13:32 -0600

    clean up codes on files

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-
 router.get('/:id', (req, res) => {
 
   const movieId = req.params.id;
--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,22 +3,6 @@ const router = express.Router();
 const axios = require('axios');
 
 // 	Parameter type is 't' = Movie title to search for.
-// router.get('/:t', (req,res) => {
-//     const searchTerms  = req.params.t;
-//     console.log('searchTerms', searchTerms);
-//     axios.get(`http://www.omdbapi.com/?t=${searchTerms}&apikey=${process.env.OMDb_API_KEY}`
-//     )
-//     .then ((response) => {
-//         console.log('response', response );
-//         // console.log('response.data', response.data );
-//         res.send(response.data);
-//     })
-//     .catch((error) => {
-//         console.log('ERROR IN OMDb API GET', error); 
-//     });
-// })
-
-
 router.get('/:t', (req, res) => {
     const searchTerms = req.params.t;
     axios.get(`http://www.omdbapi.com/?t=${searchTerms}&apikey=${process.env.OMDb_API_KEY}`)
